c6161e1ae22bc89634d00caf01c0f0722fc94a71
RIFAI_R <rifai-rayan@hotmail.com>
2026-02-06 16:32:31 +0100

    Clean Folder

--- Assets/SimpleUI/ScriptableObj/SO_Screen.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SO_Screen", menuName = "Scriptable Objects/SO_Screen")]
-public class SO_Screen : ScriptableObject
-{
-    public GameObject prefab;
-    public List<GameObject> prefabsScreen = new List<GameObject>();
-}
